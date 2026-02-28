1984c94980e16cac4ffd977c4a114d11fc15a17b
dgi314 <dani.daniel.gift@gmail.com>
2026-02-28 14:37:05 -0500

    add gradient descnt

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'rewrite-graddescent',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/graddescent' || req.url === '/graddescent/') {
+            req.url = '/graddescent.html';
+          }
+          next();
+        });
+      }
+    }
+  ],
   base: '/digital-physics-lab/'
 })
 
